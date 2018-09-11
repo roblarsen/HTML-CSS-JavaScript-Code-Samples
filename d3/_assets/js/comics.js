6826a023863588cd6ff0ba3428c8f3249b9bc250
Rob Larsen <rob@htmlcssjavascript.com>
2018-09-11 16:30:33 -0400

    ugly demo

--- d3/_assets/js/comics.js
+++ d3/_assets/js/comics.js
@@ -1,19 +1,48 @@
 
-const width = 1000,
-  height = 600;
+const margin = {top: 100, right: 20, bottom: 100, left: 100},
+width = 1440 - margin.left - margin.right,
+height = 768 - margin.top - margin.bottom;
 
-/*let svg = d3.select("#target").append("svg")
-  .attr("width", width)
-  .attr("height", height)
-  .append("g")
-  .attr("transform", `translate(${width / 2},${height / 2})`);*/
+let svg = d3.select("#target").append("svg")
+.attr("width", width + margin.left + margin.right)
+.attr("height", height + margin.top + margin.bottom);
+let parseTime = d3.timeParse("%Y-%m-%d");
 
+let x = d3.scaleTime()
+    .rangeRound([0, width]);
+
+let y = d3.scaleLinear()
+    .rangeRound([height, 0]);
+var valueline = d3.line()
+    .x(function (d) { return x(d.date); })
+    .y(function (d) { return y(d.price); });
 d3.json("data/books.json").then((data) => {
-  console.log(data)
-  d3.select("#target").selectAll("p")
-    .data(data.sales)
-    .enter()
-    .append("p")
-    .text((d)=>{ return `${d.date} ${d.title} ${d.issue} ${d.grade} ${d.note} ${d.venue} ${d.price}`});
+    console.log(data)
+    data.sales = data.sales.reverse();
+
+    data.sales.forEach(function (d) {
+        d.date = parseTime(d.date);
+        d.price = + d.price;
+    });
+    console.log(data)
+
+    x.domain(d3.extent(data.sales, function (d) { return d.date; }));
+    y.domain([0, d3.max(data.sales, function (d) { console.log(d.price);return d.price; })]);
+    let g = svg.append("g").attr("transform", `translate(${margin.top},${margin.left})`)
+
+    g.append("path")
+        .data([data.sales])
+        .attr("class", "line")
+        .attr("d", valueline);
+
+
+    g.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+
+    g.append("g")
+        .call(d3.axisLeft(y));
 
 });
+
